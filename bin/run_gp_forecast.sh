a49b197650d89e65caceb1152055bab6503dc92d
Charles Naylor <cnaylor@gmail.com>
2017-11-06 14:18:30 -0500

    Added csv dump of y covariance matrix. Updated Backtesting after additional runs came in (no actual change in conclusions, but we're down to 4 weeks that need rerunning).

--- bin/run_gp_forecast.sh
+++ bin/run_gp_forecast.sh
@@ -3,7 +3,7 @@ for i in {1..5}
 do
   ./gp_forecasts sample \
   num_samples=500 num_warmup=500 \
-  random seed=8 \
+  random seed=867530$i \
   id=$i data file=../data/weekly/gp_$1.dat \
   output file=samples_$1_$i &
 done
